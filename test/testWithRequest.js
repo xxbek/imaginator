3fa7050ab2763492ba7de72f3381fd30790eecc9
xxbek <kika.aubekerow@gmail.com>
2023-08-18 19:02:51 +0300

    change test output

--- test/testWithRequest.js
+++ test/testWithRequest.js
@@ -3,11 +3,11 @@ import axios from "axios";
 async function sendRequests() {
     const url = 'http://0.0.0.0:8080/handle/send';
 
-    const REQUESTS_NUMBER = 30
+    const REQUESTS_NUMBER = 50
 
     const requests = [];
     for (let i = 0; i < REQUESTS_NUMBER; i++) {
-        const randNum = getRandomInt(1, 20);
+        const randNum = getRandomInt(-5, 33);
         const requestBody = { proceedNum: randNum };
         const requestPromise = axios.post(url, requestBody);
         requests.push(requestPromise);
@@ -16,8 +16,9 @@ async function sendRequests() {
     try {
         const responses = await Promise.all(requests);
         responses.forEach((response, index) => {
-            console.log(`Request ${index + 1} sent with proceedNum: ${response.config.data}`);
-            console.log('Response:', response.data);
+            const responseBody = JSON.stringify(response.data)
+            console.log(`#${index + 1}: Fibonacci sequence number  ${response.config.data} (proceedNum) is: ${responseBody}`);
+            console.log('Response:', );
         });
     } catch (error) {
         console.error('Request failed:', error.message);
